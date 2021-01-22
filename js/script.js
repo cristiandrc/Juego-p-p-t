d35620414f083b6680ab328ccb36b709371dddae
cristian <crisdarroca@hotmail.com>
2021-01-21 21:28:10 -0500

    Se modifico el msm Final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 // this.secuencia = Array(ULTIMO_NIVEL).fill(0).map(n =>Math.floor(Math.random() * 4))
+// document.getElementById("msmUser").src="https://piedra-papel-tijera.com/images/piedra.png";
 
 const piedra = document.getElementById('piedra');
 const papel = document.getElementById('papel');
 const tijera = document.getElementById('tijera');
 const btnEmpezar = document.getElementById('btnEmpezar');
 const btnContenedor = document.getElementById('btnContenedor');
-
-var numero;
+const btnReiniciar = document.getElementById('btnReiniciar');
+const msmFinal = document.querySelector('.msmContainer');//usando la clase
+const tituloMsm_win = document.getElementById('tituloMsm_win');
+const tituloMsm_lose = document.getElementById('tituloMsm_lose');
+const tituloMsm_tie = document.getElementById('tituloMsm_tie');
 var maquina;
 var usuario;
 
 btnEmpezar.addEventListener('click', juego);
+btnReiniciar.addEventListener('click', reiniciarJuego);
 // piedra.addEventListener('click',iniciarJuego);
 // papel.addEventListener('click',iniciarJuego);
 // tijera.addEventListener('click',iniciarJuego);
@@ -73,6 +78,42 @@ function resultado(user,cpu){
         empate()
     }
 }
+function imgUser(){
+    switch (usuario) {
+        case 'piedra':
+            document.getElementById("msmUser").src="https://piedra-papel-tijera.com/images/piedra.png";
+            imgCpu()
+            break;
+        case 'papel':
+            document.getElementById('msmUser').src="https://piedra-papel-tijera.com/images/papel.png";
+            imgCpu()
+            break;
+        case 'tijera':
+            document.getElementById('msmUser').src="https://piedra-papel-tijera.com/images/tijeras.png";
+            imgCpu()
+
+        default:
+            break;
+    }
+    
+
+}
+
+function imgCpu(){
+    switch (maquina) {
+        case 'piedra':
+            document.getElementById("msmCpu").src="https://piedra-papel-tijera.com/images/piedra.png";
+            break;
+        case 'papel':
+            document.getElementById('msmCpu').src="https://piedra-papel-tijera.com/images/papel.png";
+            break;
+        case 'tijera':
+            document.getElementById('msmCpu').src="https://piedra-papel-tijera.com/images/tijeras.png";
+
+        default:
+            break;
+    }
+}
 
 function numeroRadon(){
     numero = Math.floor(Math.random() * 3)
@@ -100,13 +141,28 @@ function transformarNumero(){
 
 
 function ganas(){
-    alert('ganastes')
+    imgUser()
+    tituloMsm_win.classList.remove('hide')
+    msmFinal.classList.remove('hide')
+    // document.getElementById('msmFinal').innerHTML = 'FIERA GANASTE ;)';
 }
 
 function perdio(){
-    alert('Pierdes')
+    imgUser()
+    tituloMsm_lose.classList.remove('hide')
+    msmFinal.classList.remove('hide')
 }
 
 function empate(){
-    alert('EMPATE CRACK')
+    imgUser()
+    tituloMsm_tie.classList.remove('hide')
+    msmFinal.classList.remove('hide')
+}
+
+function reiniciarJuego(){
+    tituloMsm_win.classList.add('hide')
+    tituloMsm_lose.classList.add('hide')
+    tituloMsm_tie.classList.add('hide')
+    msmFinal.classList.add('hide');
+    juego();
 }
